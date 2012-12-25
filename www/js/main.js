36488ab99d914c01979728b9f7651d29128169cc
Itou Tatsuhiko <alivecast@aciMac.local>
2012-12-25 23:41:49 +0900

    121225 2341 mantis修正 save

--- www/js/main.js
+++ www/js/main.js
@@ -89,11 +89,11 @@ function currency(str) {
 	return num;
 }
 
-
 // fnが関数ならそのまま返し、文字列なら関数名にして返す
 function $FN(fn) {
-    return (typeof fn == 'function') ? fn : Function('x', 'return ' + fn);
+	return ( typeof fn == 'function') ? fn : Function('x', 'return ' + fn);
 }
+
 // ファイル書込処理
 function setFileOld(fname, jsonObj) {
 	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, write, setError1);
@@ -109,24 +109,28 @@ function setFileOld(fname, jsonObj) {
 				writer.onerror = function(evt) {
 					console.log('書き込みに失敗しました。' + evt.toString());
 				};
-//				// 内容を書き込み
-//				var jsonText = JSON.stringify(jsonObj);
+				//				// 内容を書き込み
+				//				var jsonText = JSON.stringify(jsonObj);
 				// 暗号化
-                var encText = encAesObj(jsonObj);
-                console.log('setFileOld::encText: ' + encText);
-                writer.write(encText);            
-//				writer.write(jsonText);
+				var encText = encAesObj(jsonObj);
+				console.log('setFileOld::encText: ' + encText);
+				writer.write(encText);
+				//				writer.write(jsonText);
 			}, setError2);
 		}, setError3);
 	}
+
 }
-function setError1(error){
+
+function setError1(error) {
 	console.log('setError1: ' + error);
 }
-function setError2(error){
+
+function setError2(error) {
 	console.log('setError2: ' + JSON.stringify(error));
 }
-function setError3(error){
+
+function setError3(error) {
 	console.log('setErro3r: ' + error);
 }
 
@@ -147,14 +151,14 @@ function setFile(fname, jsonObj, okfn, errfn) {
 				};
 				// 内容を書き込み
 				// var jsonText = JSON.stringify(jsonObj);
-            	// writer.write(jsonText);
-                // callback = $FN(okfn);
-                // return callback(jsonText);
+				// writer.write(jsonText);
+				// callback = $FN(okfn);
+				// return callback(jsonText);
 
-                // 暗号化
-                var encText = encAesObj(jsonObj);
-                console.log('setFile::encText: ' + encText);
-                writer.write(encText);
+				// 暗号化
+				var encText = encAesObj(jsonObj);
+				console.log('setFile::encText: ' + encText);
+				writer.write(encText);
 				callback = $FN(okfn);
 				return callback(encText);
 
@@ -162,15 +166,16 @@ function setFile(fname, jsonObj, okfn, errfn) {
 		}, setNG);
 	}
 
-	function setNG(error){
+	function setNG(error) {
 		console.log("ファイルを書き込めませんでした。");
 		callback = $FN(errfn);
 		return callback()
 	}
+
 }
 
 // ファイル読み込み処理(読み込み成功ならokfn関数を実行、失敗ならerrfn関数を実行)
-function getFile(fname,okfn,errfn) {
+function getFile(fname, okfn, errfn) {
 	var text = {};
 	window.requestFileSystem(LocalFileSystem.PERSISTENT, 0, read, noFile);
 	function read(fileSystem) {
@@ -180,16 +185,16 @@ function getFile(fname,okfn,errfn) {
 				reader.onloadend = function(evt) {
 					// ここに読み込み完了後の処理を書く
 					text = evt.target.result;
-                    if(text){
-                    	console.log("getFile::正常にファイルを読み込みました。");
-                        var encText = text;
-                        var obj = decAesObj(encText);
-                        callback = $FN(okfn);
-                        return callback(obj);
-                    }else{
-                    	console.log("getFile::正常にファイルを読み込みましたが、データがありません。");
-                        noFile();
-                    }
+					if (text) {
+						console.log("getFile::正常にファイルを読み込みました。");
+						var encText = text;
+						var obj = decAesObj(encText);
+						callback = $FN(okfn);
+						return callback(obj);
+					} else {
+						console.log("getFile::正常にファイルを読み込みましたが、データがありません。");
+						noFile();
+					}
 
 				};
 				reader.readAsText(file, "utf-8");
@@ -197,14 +202,13 @@ function getFile(fname,okfn,errfn) {
 		}, noFile);
 	}
 
-	function noFile(error){
+	function noFile(error) {
 		console.log("ファイルを読み込めませんでした。");
 		callback = $FN(errfn);
 		return callback()
 	}
 
 }
-
 
 // ファイル削除
 function delFile(fname) {
@@ -229,105 +233,120 @@ function getonOrdering(callfn) {
 	// ファイルの読み込み
 	console.log("Start ordering");
 	var fname = 'ordering';
-	getFile(fname,getonOrderingOK,getonOrderingNG);
+	getFile(fname, getonOrderingOK, getonOrderingNG);
 
-	function getonOrderingOK(json){
+	function getonOrderingOK(json) {
 		//alert('getonOrdering: ' + JSON.stringify(json));
 		console.log("OK : ordering");
-		getonMSG(json,callfn);
+		getonMSG(json, callfn);
 	}
 
-	function getonOrderingNG(){
+	function getonOrderingNG() {
 		console.log("NG : ordering");
-		location.href="index.html";
+		location.href = "index.html";
 		return false;
 	}
+
 }
 
 // 超重要メッセージの出力
-function getonMSG(json,callfn) {
+function getonMSG(json, callfn) {
 	// URLの取得
 	var callback = $FN(callfn);
 	$.ajax({
-		type: "POST",
-		url: "http://exorder.jp/app/msg.php",
-		data:"id1=alivecast&id2=sugoi&app_code="+json.app_code+"&app_ver="+json.app_ver,
-		success: function(data){
-			if(data){
+		type : "POST",
+		url : "http://exorder.jp/app/msg.php",
+		data : "id1=alivecast&id2=sugoi&app_code=" + json.app_code + "&app_ver=" + json.app_ver,
+		success : function(data) {
+			if (data) {
 				console.log("重要ＭＳＧ");
 				button = 'OK';
 				title = '【重要メッセージ】';
 				navigator.notification.alert(data, callback(json), title, button);
-			}else{
+			} else {
 				return callback(json);
 			}
 		},
-		error: function(){
-			
-	$.ajax({
-		type: "POST",
-		url: "http://www.alivecast.jp/exorder/msg.php",
-		data:"id1=alivecast&id2=sugoi&app_code="+json.app_code+"&app_ver="+json.app_ver,
-		success: function(data){
-			if(data){
-				console.log("重要ＭＳＧ");
-				button = 'OK';
-				title = '【重要メッセージ】';
-				navigator.notification.alert(data, callback(json), title, button);
-			}else{
-				return callback(json);
-			}
-		},
-		error: function(){
-			return callback(json);
-		}
-	});
-	
+		error : function() {
+			$.ajax({
+				type : "POST",
+				url : "http://www.alivecast.jp/exorder/msg.php",
+				data : "id1=alivecast&id2=sugoi&app_code=" + json.app_code + "&app_ver=" + json.app_ver,
+				success : function(data) {
+					if (data) {
+						console.log("重要ＭＳＧ");
+						button = 'OK';
+						title = '【重要メッセージ】';
+						navigator.notification.alert(data, callback(json), title, button);
+					} else {
+						return callback(json);
+					}
+				},
+				error : function() {
+					return callback(json);
+				}
+			});
 		}
 	});
 }
 
 // 通信エラーメッセージ文
-function networdERR(callfn,title) {
-	message = '通信が混み合っているか、通信環境がよくないため、もうしばらくお待ちしてから、接続しなおしてください。';
+function networdERR(callfn, title) {
+	message = '通信が混み合っているか、通信環境がよくないため、しばらく時間をおいて操作を行ってください。';
 	button = 'OK';
 	callback = $FN(callfn);
-	if(title){
-	}else{
+	if (title) {
+	} else {
 		title = '通信エラー';
 	}
-	navigator.notification.alert(message, callback, title, button);
+	// navigator.notification.alert(message, callback, title, button);
+	navigator.notification.alert(
+		message, 
+		function(){
+			window.location.href = 'netError.html'
+		}, 
+		title, 
+		button
+	);
 }
 
 // 購入時の通信エラーメッセージ文
-function networdBuyERR(callfn,title) {
+function networdBuyERR(callfn, title) {
 	message = '通信が込み合っているか通信環境が良くないため、しばらく時間をおいて再度購入を行ってください。';
 	button = 'OK';
 	callback = $FN(callfn);
-	if(title){
-	}else{
+	if (title) {
+	} else {
 		title = '通信エラー';
 	}
-	navigator.notification.alert(message, callback, title, button);
+	// navigator.notification.alert(message, callback, title, button);
+	navigator.notification.alert(
+		message, 
+		function(){
+			window.location.href = 'netError.html'
+		}, 
+		title, 
+		button
+	);
 }
 
-
 // ネットワーク状態を調べる
-function check_network(){
+function check_network() {
 	var domain = 'speed-order.jp';
 	navigator.network.isReachable(domain, reachableCallback);
 }
+
 // ネットワーク状態を受け取るlコールバック
 function reachableCallback(reachability) {
-    // 現状では各プラットフォーム間でのreachabilityのフォーマットに関しての一貫性はありません
-    var networkState = reachability.code || reachability;
+	// 現状では各プラットフォーム間でのreachabilityのフォーマットに関しての一貫性はありません
+	var networkState = reachability.code || reachability;
 
-    var states = {};
-    states[NetworkStatus.NOT_REACHABLE]                      = '接続可能なネットワークが見つかりません';
-    states[NetworkStatus.REACHABLE_VIA_CARRIER_DATA_NETWORK] = 'データ接続';
-    states[NetworkStatus.REACHABLE_VIA_WIFI_NETWORK]         = 'WiFi接続';
+	var states = {};
+	states[NetworkStatus.NOT_REACHABLE] = '接続可能なネットワークが見つかりません';
+	states[NetworkStatus.REACHABLE_VIA_CARRIER_DATA_NETWORK] = 'データ接続';
+	states[NetworkStatus.REACHABLE_VIA_WIFI_NETWORK] = 'WiFi接続';
 
-    console.log('接続の形式: ' + states[networkState]);
+	console.log('接続の形式: ' + states[networkState]);
 }
 
 // 郵便番号の取得 //////////////////////////////////////////////////////////////////////
@@ -338,9 +357,10 @@ function getAddress() {
 
 function setPref(data) {
 	$('#ordering\\[customer_pref\\]').val(data);
-//	var select = $('#ordering\\[customer_pref\\]');
-//	select.val(40);
+	//	var select = $('#ordering\\[customer_pref\\]');
+	//	select.val(40);
 }
+
 function setAddress(data) {
 	$('#ordering\\[customer_address1\\]').val(data);
 }
@@ -350,15 +370,15 @@ function getCarriage(price, carriage, non_carriage_price) {
 	var amount = document.getElementById('#order_amount').value;
 	var total = (price * amount) + carriage;
 	var neoCarriage = carriage;
-	if (total >= non_carriage_price){
+	if (total >= non_carriage_price) {
 		total = (price * amount);
 		neoCarriage = 0;
 	}
 	// 桁区切り
 	str = String(total);
 	var num = new String(str).replace(/,/g, "");
-	while(num != (num = num.replace(/^(-?\d+)(\d{3})/, "$1,$2")));
-//	return num;
+	while (num != ( num = num.replace(/^(-?\d+)(\d{3})/, "$1,$2")));
+	//	return num;
 	// 送料無料を反映
 	setCarriage(neoCarriage);
 	document.getElementById('#total').innerHTML = num + '円(税込)';
@@ -368,8 +388,8 @@ function setCarriage(carriage) {
 	// 桁区切り
 	var str = String(carriage);
 	var num = new String(str).replace(/,/g, "");
-	while(num != (num = num.replace(/^(-?\d+)(\d{3})/, "$1,$2")));
-	if (num = '0' ){
+	while (num != ( num = num.replace(/^(-?\d+)(\d{3})/, "$1,$2")));
+	if ( num = '0') {
 		num = '無料';
 	} else {
 		num + '円';
@@ -377,64 +397,62 @@ function setCarriage(carriage) {
 	document.getElementById('#carriage').innerHTML = String(num);
 }
 
-function timeout(){
-	 // タイムアウト処理設定
-     // $.ajaxSetup({timeout: 3000});
-     
-}
-
 // AES128 encode CBC ------------------------------------------------------------
 function encAesObj(obj) {
-    console.log('::encAesObj::');
-    var key = CryptoJS.enc.Hex.parse(randPassWord(32));
-    var iv  = CryptoJS.enc.Hex.parse(randPassWord(32));
-    var str = JSON.stringify(obj);
-    var encrypted = CryptoJS.AES.encrypt(str, key, { iv: iv });
-    var enc = {};
-    enc['key'] = encrypted.key;
-    enc['iv'] = encrypted.iv;
-    // enc['enc'] = encrypted;
-    enc['enc'] = encrypted.ciphertext.toString(CryptoJS.enc.Base64);
-    var encText = encBase64(JSON.stringify(enc));
-    return encText;
+	console.log('::encAesObj::');
+	var key = CryptoJS.enc.Hex.parse(randPassWord(32));
+	var iv = CryptoJS.enc.Hex.parse(randPassWord(32));
+	var str = JSON.stringify(obj);
+	var encrypted = CryptoJS.AES.encrypt(str, key, {
+		iv : iv
+	});
+	var enc = {};
+	enc['key'] = encrypted.key;
+	enc['iv'] = encrypted.iv;
+	// enc['enc'] = encrypted;
+	enc['enc'] = encrypted.ciphertext.toString(CryptoJS.enc.Base64);
+	var encText = encBase64(JSON.stringify(enc));
+	return encText;
 }
 
 // AES128 decode CBC -------------------------------------------------------------
 function decAesObj(encText) {
-    console.log('::decAesObj::encText: ' + encText);
-    // base64エンコードされてないファイル読み込みの暫定処置
-    if(encText.match(/^{/)){
-        var enc = JSON.parse(encText);
-    } else {
-        var enc = JSON.parse(decBase64(encText));
-    }
-    if(enc.key || enc.enc){
-    	console.log('Key ari');
-    	var key = enc.key;
-	    var iv = enc.iv;
-	    var enc = enc.enc;
-	    var objText = CryptoJS.AES.decrypt(enc, key, { iv: iv });
+	console.log('::decAesObj::encText: ' + encText);
+	// base64エンコードされてないファイル読み込みの暫定処置
+	if (encText.match(/^{/)) {
+		var enc = JSON.parse(encText);
+	} else {
+		var enc = JSON.parse(decBase64(encText));
+	}
+	if (enc.key || enc.enc) {
+		console.log('Key ari');
+		var key = enc.key;
+		var iv = enc.iv;
+		var enc = enc.enc;
+		var objText = CryptoJS.AES.decrypt(enc, key, {
+			iv : iv
+		});
 		objText = objText.toString(CryptoJS.enc.Utf8);
 		var obj = JSON.parse(objText);
-    }else{
-    	console.log('Key nashi');
-    	var obj = enc;
-    }
-    console.log("objText: " + obj);
-    return obj;
+	} else {
+		console.log('Key nashi');
+		var obj = enc;
+	}
+	console.log("objText: " + obj);
+	return obj;
 }
 
 // ランダム文字列 -------------------------------------------------------------
 function randPassWord(length) {
-    length = length || '';
-    var rand = 'abcdefghijklmnopqrstuvwxyz' + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + '0123456789';
-    // var rand = 'abcdef' + '0123456789';
-    rand = rand.split('');
-    var pass = '';
-    for (var i = 0; i < length; i++) {
-        pass += rand[Math.floor(Math.random() * rand.length)];
-    }
-    return pass;
+	length = length || '';
+	var rand = 'abcdefghijklmnopqrstuvwxyz' + 'ABCDEFGHIJKLMNOPQRSTUVWXYZ' + '0123456789';
+	// var rand = 'abcdef' + '0123456789';
+	rand = rand.split('');
+	var pass = '';
+	for (var i = 0; i < length; i++) {
+		pass += rand[Math.floor(Math.random() * rand.length)];
+	}
+	return pass;
 }
 
 function getLocalStorage(fname) {
@@ -465,9 +483,9 @@ function popup(type, title, message) {
 	new $pop(message, {
 		type : type,
 		title : title,
-		width: 220,
-		height: 120,
-		close:false,
-		modal: true
+		width : 220,
+		height : 120,
+		close : false,
+		modal : true
 	});
 }
